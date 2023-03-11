77f2555dacab749891d8b53c66d0ef37666ad724
Josh Gulledge <jgulledge19@gmail.com>
2023-03-11 15:41:02 -0700

    Set up index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+function Index() {
 
-const Index = () => {
   return (
-    <div>Index</div>
+    <div className="showcard">  
+          <div className="intro">
+              <p>Your vehicle takes care of you every day on the road. It's important for<br></br>for you to keep up on your vehicles maintenance.</p>
+              <img src="https://cdn.dealeraccelerate.com/stlouis/1/1595/50686/1920x1440/1969-dodge-charger" alt=""/>
+              <p>This application is a great way for you to keep track of your vehicles needs.</p>
+            
+          </div>        
+    </div>
+ 
   )
 }
 
